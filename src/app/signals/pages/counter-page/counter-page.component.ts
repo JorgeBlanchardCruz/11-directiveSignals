98a6fccf5ca84ff95a406b6ce08f2f08651dac15
Jorge Blanchard Cruz <rothgard@live.com>
2024-07-04 19:24:47 +0100

    computed signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
 })
 export class CounterPageComponent {
 
+    public counter = signal(10);
+    public squareCounter = computed( () => this.counter() ** 2);
+
+    increaseBy(value: number) {
+        this.counter.update( current => current + value);
+    }
 }
